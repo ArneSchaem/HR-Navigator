61aa7d8333c5cd11d30729a8d8a9b38815ba0d75
ArneSchaem <97231706+ArneSchaem@users.noreply.github.com>
2023-10-12 15:05:41 +0200

    add filter

---
+++ main.js
@@ -0,0 +1,42 @@
+"use strict";
+var filtersearch = {};
+
+filtersearch.getElements = function (id) {
+  if (typeof id == "object") {
+    return [id];
+  } else {
+    return document.querySelectorAll(id);
+  }
+};
+filtersearch.filterHTML = function(id, sel, filter) {
+  var a, b, c, i, ii, iii, hit;
+  a = filtersearch.getElements(id);
+  for (i = 0; i < a.length; i++) {
+    b = a[i].querySelectorAll(sel);
+    for (ii = 0; ii < b.length; ii++) {
+      hit = 0;
+      if (b[ii].innerText.toUpperCase().indexOf(filter.toUpperCase()) > -1) {
+        hit = 1;
+      }
+      c = b[ii].getElementsByTagName("*");
+      for (iii = 0; iii < c.length; iii++) {
+        if (c[iii].innerText.toUpperCase().indexOf(filter.toUpperCase()) > -1) {
+          hit = 1;
+        }
+      }
+      if (hit == 1) {
+        b[ii].style.display = "";
+      } else {
+        b[ii].style.display = "none";
+      }
+    }
+  }
+};
+
+
+
+
+
+
+
+
